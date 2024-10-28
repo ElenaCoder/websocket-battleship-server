39426e8397f4de70077b07aae83772723bcf116a
ElenaCoder <97116051+ElenaCoder@users.noreply.github.com>
2024-10-28 23:20:27 +0200

    feat: add file with constants

---
+++ src/http_server/constants.js
@@ -0,0 +1,14 @@
+export const MESSAGE_TYPES = {
+    REG: 'reg',
+    UPDATE_WINNERS: 'update_winners',
+    CREATE_ROOM: 'create_room',
+    ADD_USER_TO_ROOM: 'add_user_to_room',
+    UPDATE_ROOM: 'update_room',
+    CREATE_GAME: 'create_game',
+    ADD_SHIPS: 'add_ships',
+    START_GAME: 'start_game',
+    ATTACK: 'attack',
+    TURN: 'turn',
+    FINISH: 'finish',
+    RANDOM_ATTACK: 'randomAttack'
+};
